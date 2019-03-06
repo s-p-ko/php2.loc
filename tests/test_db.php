164f27c654afae72372a6dfc5d303181c5db5bda
Serhiy Kotsyuba <s-p-ko@ukr.net>
2019-03-06 09:14:44 +0200

    Some testing

--- tests/test_db.php
+++ tests/test_db.php
@@ -24,12 +24,12 @@ assert( 'array' === gettype( $db->query($sql) ) );
 $sql = 'SELECT * FROM news WHERE id=:id';
 assert( 'object' === gettype( $db->query($sql, [':id' => 1], $classArticle)[0] ) );
 assert( 'array' === gettype( $db->query($sql, [':id' => 1])[0] ) );
-assert( true === empty( $db->query($sql, [':id' => 20000])[0] ) );
+assert( true === empty( $db->query($sql, [':id' => 4000])[0] ) );
 
 $sql = 'SELECT * FROM users WHERE id=:id';
-assert( 'object' === gettype( $db->query($sql, [':id' => 1], $classUser)[0] ) );
-assert( 'array' === gettype( $db->query($sql, [':id' => 1])[0] ) );
-assert( true === empty( $db->query($sql, [':id' => 20000])[0] ) );
+assert( 'object' === gettype( $db->query($sql, [':id' => 2], $classUser)[0] ) );
+assert( 'array' === gettype( $db->query($sql, [':id' => 2])[0] ) );
+assert( true === empty( $db->query($sql, [':id' => 4000])[0] ) );
 //var_dump($db->query($sql, [':id' => 1], $classUser)[0]);
 //var_dump($db->query($sql, [':id' => 1])[0]);
 //var_dump($db->query($sql, [':id' => 10], $classUser)[0]);
@@ -39,3 +39,6 @@ $sql = 'INSERT INTO news (title, content) VALUES(:title, :content)';
 assert( true === $db->execute($sql, [':title' => 'Title test', ':content' =>
         'Content test']) );
 
+$sql = 'INSERT INTO users (email, password) VALUES(:email, :password)';
+assert( true === $db->execute($sql, [':email' => 'test@test.com', ':password' =>
+        '121212']) );
