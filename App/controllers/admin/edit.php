aa4ced759496a7e699c69d810d36f035e690aa3f
Serhiy Kotsyuba <s-p-ko@ukr.net>
2019-03-12 17:27:32 +0200

    Minor changes 3

--- App/controllers/admin/edit.php
+++ App/controllers/admin/edit.php
@@ -3,8 +3,7 @@ require_once __DIR__ . '/../../../autoload.php';
 
 use App\Models\Article;
 
-$data = Article::findById($_GET['id']);
-
-if (false !== $data) {
+if (isset($_GET['id'])) {
+    $data = Article::findById($_GET['id']);
     include __DIR__ . '/../../../templates/admin/edit.php';
 }
--- App/controllers/admin/rewrite.php
+++ App/controllers/admin/rewrite.php
@@ -3,12 +3,12 @@ require_once __DIR__ . '/../../../autoload.php';
 
 use App\Models\Article;
 
-$article = Article::findById($_POST['id']);
-
-if (false !== $article ) {
+if (isset($_POST['id'])) {
+    $article = Article::findById($_POST['id']);
     $article->title = $_POST['title'];
     $article->content = $_POST['content'];
     $article->save();
 }
+
 header('Location: /App/controllers/admin/');
 exit();
