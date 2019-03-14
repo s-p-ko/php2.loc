c22da41ffc78b34b7882aca59ead83b37b6258eb
Serhiy Kotsyuba <s-p-ko@ukr.net>
2019-03-14 16:14:06 +0200

    hw3 3rd var. Block for the future 'Error 404'

--- App/controllers/admin/edit.php
+++ App/controllers/admin/edit.php
@@ -7,8 +7,10 @@ use App\View;
 $view = new View();
 $view->data = Article::findById($_GET['id']);
 
-if (false == $view->data) {
-    header('Location: /App/controllers/admin/');
-    exit();
-}
+//Block for the future answer 'Error 404'
+//if (false == $view->data) {
+//    header('Location: /App/controllers/admin/');
+//    exit();
+//}
+
 $view->display(__DIR__ . '/../../../templates/admin/edit.php');
--- App/controllers/article.php
+++ App/controllers/article.php
@@ -7,8 +7,9 @@ use App\View;
 $view = new View();
 $view->article = Article::findById($_GET['id']);
 
-if (false == $view->article) {
-    header('Location: /');
-    exit();
-}
+//Block for the future answer 'Error 404'
+//if (false == $view->article) {
+//    header('Location: /');
+//    exit();
+//}
 $view->display(__DIR__ . '/../../templates/article.php');
